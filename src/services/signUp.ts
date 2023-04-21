a674c623be614e1fba9a87482bbe2c49d5b7e91d
mintuka <mintuka2015@gmail.com>
2023-04-21 09:59:49 +0300

    Completed sign-up service

---
+++ src/services/signUp.ts
@@ -0,0 +1,22 @@
+import {Request, Response} from 'express'
+import User, { IUser } from '../models/User'
+import bcrypt from 'bcrypt'
+import { createToken } from './createToken'
+
+export const signUp = async(req: Request, res: Response) => {
+    try{
+        const {email, password} = req.body
+        const isExistingUser = await User.findOne({email})
+        if (isExistingUser){
+            return res.status(400).json({message: 'email is already registered'})
+        }
+        const salt = await bcrypt.genSalt()
+        const hash = await bcrypt.hash(password,salt)
+        const user: IUser = await User.create({...req.body,password:hash})
+        const token = createToken(user._id)
+        return res.status(200).json({email, token})
+    }catch(err){
+        return res.status(400).json({message: err})
+    }
+
+}
